b02289833e7a97c8965769f414675932cee49a30
Emmanuel Wang <clochard93@gmail.com>
2015-10-15 10:35:29 -0400

    updated oct 15 2015

---
+++ OTHER_DP_Min_imbalance.cpp
@@ -0,0 +1,50 @@
+/*************************************************************************
+	> File Name: DP_Min_Imbalance.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: Thu Oct 15 00:51:24 2015
+ ************************************************************************/
+
+#include<cmath>
+#include<iostream>
+using namespace std;
+
+#define MAX 100
+
+int main() {
+    // init
+    int n = 9;
+    int k = 2;
+    int f[n + 1][k + 2];
+    int a[n + 1];
+    int sum[n + 1];
+    int ave = 0;
+
+    for (int i = 0;i < n + 1; ++i) {
+        a[i] = i;
+        sum[i] = sum[i - 1] + a[i];
+    }
+    ave = sum[n] / (k + 1);
+
+    for (int i = 1; i < n + 1; ++i) {
+        f[i][1] = abs(sum[i] - ave);
+    }
+    for (int j = 1; j < k + 2; ++j) {
+        f[1][j] = abs(a[1] - ave);
+    }
+    
+    // dp
+    for (int i = 2; i < n + 1; ++i) {
+        for (int j = 2; j < k + 2; ++j) {
+            f[i][j] = MAX;
+            for (int l = 1; l < i + 1; ++l) {
+                int imb = max(f[l][j - 1], abs(sum[i] - sum[l] - ave));
+                if (imb < f[i][j]) f[i][j] = imb;
+            }
+            //cout << f[i][j] << endl;
+        }
+    }
+
+    cout << f[n][k + 1] << endl;
+return 0;
+}
--- README.md
+++ README.md
@@ -15,3 +15,12 @@ LINT_DP_longest_com_subseq.cpp:
 
     LCS is defined as https://en.wikipedia.org/wiki/Longest_common_subsequence_problem.  
 
+
+OTHER_DP_Min_Imbalance.cpp:
+
+    Given an input of array a and a partition k, we split a[n] into k+1 subarray. 
+    Also, let an Imbalance of Array defined as the largest distance from the sum of any
+    subarray to the average of subarray (sum(a[n])/(k+1)).
+
+    Output: a number of the Imbalance of Array s.t. its the smallest among all
+    possible partitions given a[n] & k.
